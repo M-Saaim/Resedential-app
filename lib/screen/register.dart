7107c5e271d8b27b085b9eff8b21b605aff9c7ba
Mohammad Saaim Qureshi <qsaaim123@gmail.com>
2023-03-30 20:21:39 +0530

    register

--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:resedentialapp/screen/homepage.dart';
 
 class Register extends StatefulWidget {
@@ -11,6 +12,9 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   late String name;
   late String email;
+  late String phone;
+  late String house;
+  late String address;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -69,6 +73,111 @@ class _RegisterState extends State<Register> {
                                 Icons.person,
                                 color: Color.fromARGB(255, 170, 0, 0),
                               ),
+                            )),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.02,
+                        ),
+                        TextField(
+                            keyboardType: TextInputType.emailAddress,
+                            textAlign: TextAlign.center,
+                            onChanged: (value) {
+                              email = value;
+                              //Do something with the user input.
+                            },
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hoverColor:
+                                  const Color.fromRGBO(97, 167, 238, 0.684),
+                              fillColor: Colors.black12,
+                              filled: true,
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                    color: Colors.grey,
+                                    width: 2.0,
+                                  ),
+                                  borderRadius: BorderRadius.circular(9)),
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.deepPurple.shade400,
+                                    width: 2.0,
+                                  ),
+                                  borderRadius: BorderRadius.circular(9)),
+                              hintText: 'Email',
+                              alignLabelWithHint: true,
+                              label: const Icon(
+                                Icons.mail,
+                                color: Color.fromARGB(255, 170, 0, 0),
+                              ),
+                            )),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.02,
+                        ),
+                        TextField(
+                            keyboardType: TextInputType.emailAddress,
+                            textAlign: TextAlign.center,
+                            onChanged: (value) {
+                              phone = value;
+                              //Do something with the user input.
+                            },
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hoverColor:
+                                  const Color.fromRGBO(97, 167, 238, 0.684),
+                              fillColor: Colors.black12,
+                              filled: true,
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                    color: Colors.grey,
+                                    width: 2.0,
+                                  ),
+                                  borderRadius: BorderRadius.circular(9)),
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.deepPurple.shade400,
+                                    width: 2.0,
+                                  ),
+                                  borderRadius: BorderRadius.circular(9)),
+                              hintText: 'Mobile No.',
+                              alignLabelWithHint: true,
+                              label: const Icon(
+                                Icons.phone,
+                                color: Color.fromARGB(255, 170, 0, 0),
+                              ),
+                            )),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.02,
+                        ),
+                        TextField(
+                            keyboardType: TextInputType.emailAddress,
+                            textAlign: TextAlign.center,
+                            onChanged: (value) {
+                              house = value;
+                              //Do something with the user input.
+                            },
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hoverColor:
+                                  const Color.fromRGBO(97, 167, 238, 0.684),
+                              fillColor: Colors.black12,
+                              filled: true,
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                    color: Colors.grey,
+                                    width: 2.0,
+                                  ),
+                                  borderRadius: BorderRadius.circular(9)),
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.deepPurple.shade400,
+                                    width: 2.0,
+                                  ),
+                                  borderRadius: BorderRadius.circular(9)),
+                              hintText: 'Room no/Wing',
+                              alignLabelWithHint: true,
+                              label: const Icon(
+                                Icons.house_rounded,
+                                color: Color.fromARGB(255, 170, 0, 0),
+                              ),
                               // labelText: "Email"
                             )),
                         SizedBox(
@@ -106,7 +215,34 @@ class _RegisterState extends State<Register> {
                                 color: Color.fromARGB(255, 170, 0, 0),
                               ),
                               // labelText: "Email"
-                            ))
+                            )),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.02,
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const HomePAge()));
+                          },
+                          style: ElevatedButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20.0, vertical: 15.0),
+                              backgroundColor:
+                                  const Color.fromARGB(255, 170, 0, 0),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5.0))),
+                          child: Text(
+                            "Register",
+                            style: GoogleFonts.roboto(
+                                textStyle: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    letterSpacing: 1.3,
+                                    fontWeight: FontWeight.w800)),
+                          ),
+                        ),
                       ],
                     ),
                   ));
