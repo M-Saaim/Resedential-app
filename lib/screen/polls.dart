d3edaf2c40ab7e132b2ea974e5db6770181a787b
Mohammad Saaim Qureshi <qsaaim123@gmail.com>
2023-04-21 07:45:47 +0530

    v

--- lib/screen/polls.dart
+++ lib/screen/polls.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_polls/flutter_polls.dart';
 import 'package:resedentialapp/screen/homepage.dart';
-import 'package:resedentialapp/screen/voting.dart';
 
 late String email;
 late String userid;
